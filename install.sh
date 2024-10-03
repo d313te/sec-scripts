fc48c631259104ce5d62cbc3b86d6a42cd5b4d21
d313te <gh@standb.nl>
2024-10-03 13:27:04 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/d313te/sec-scripts/refs/heads/main/shell -O /usr/bin/shell
+wget https://raw.githubusercontent.com/d313te/sec-scripts/refs/heads/main/up -O /usr/bin/up
+chmod +x /usr/bin/up
+chmod +x /usr/bin/shell
