6af8243cd44acd65f6d1b360a15455c4a1a1b3e8
d313te <gh@standb.nl>
2024-10-05 12:54:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt install xclip
 wget https://raw.githubusercontent.com/d313te/sec-scripts/refs/heads/main/shell -O /usr/bin/shell
 wget https://raw.githubusercontent.com/d313te/sec-scripts/refs/heads/main/up -O /usr/bin/up
 chmod +x /usr/bin/up
